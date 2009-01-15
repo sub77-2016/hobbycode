9764641e62abcaabe9eae15775e16c26f2ed5780
ngamsaad.waipot <ngamsaad.waipot@ae0df2b8-a31b-11dd-a8c6-677c70d10fe4>
2009-01-15 06:14:17 +0000

    add AI2D

--- trunk/AI2D/intrepid-gl/examples/example_001/build.sh
+++ trunk/AI2D/intrepid-gl/examples/example_001/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-g++ main.cpp intrepid-gl.a `sdl-config --cflags --libs` -lSDL_ttf -lGL -lz
+g++ `sdl-config --cflags` -I../../intrepid_0-2_alpha  -c main.cpp
+g++ `sdl-config --libs` -o main main.o -L../../intrepid_0-2_alpha/src/ -lintrepid-gl -lSDL_ttf -lGL -lz
 
