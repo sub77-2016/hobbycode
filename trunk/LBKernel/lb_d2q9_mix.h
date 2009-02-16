8f1b919a3f071544073a16f98a6b21eb81730889
ngamsaad.waipot <ngamsaad.waipot@ae0df2b8-a31b-11dd-a8c6-677c70d10fe4>
2009-02-16 15:45:33 +0000

    rename some files

---
+++ trunk/LBKernel/lb_d2q9_mix.h
@@ -0,0 +1,73 @@
+/*
+ * Created by Waipot Ngamsaad (ngamsaad.waipot@gmail.com)
+ * Dated 25-01-2009
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LB2DMIX_H
+#define LB2DMIX_H
+
+#include "lb_d2q9.h"
+
+class mglGraph;
+
+namespace TINY_LB
+{
+	class LBD2Q9Mix : public LBD2Q9
+	{
+	public:
+		LBD2Q9Mix(int nx, int ny);
+		virtual ~LBD2Q9Mix(void);
+
+		virtual void init(void (*loader)(int x, int y, real* rho0, real& ux, real& uy));	
+		virtual void step(void);
+		virtual void run(int time);
+		virtual void writeOutput(void);
+
+	#ifdef USE_GRAPHICS
+		virtual int run(int argc, char **argv);
+	#endif
+
+	protected:
+		virtual void computeMoments(void);
+		virtual void collide(void);
+		virtual void multcollide(void);
+		virtual void f_eq(const int x, const int y, real f_out[9][2]);
+
+	#ifdef USE_GRAPHICS
+		virtual void initGUI(int argc, char **argv);
+		virtual void reinit(void);
+
+		// GUI stuff
+		int mXdim, mYdim;
+		int mQuit, mPause, mIterations, mSingleStep, mRepeat;
+	#endif
+
+		real *phi;
+		real *rho;
+		real *U;
+
+		real *omega;
+
+		real C[9][9];
+	
+	private:
+
+
+	};
+
+}
+
+#endif
---
+++ trunk/LBKernel/lb_viewer.h
@@ -0,0 +1,55 @@
+/*
+ * Created by Waipot Ngamsaad (ngamsaad.waipot@gmail.com)
+ * Dated 25-01-2009
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LBVIEWER_H
+#define LBVIEWER_H
+
+#include "lbd2q9.h"
+
+struct mglDraw;
+class mglGraph;
+
+namespace TINY_LB 
+{
+	class LBViewer : public mglDraw
+	{
+	public:
+		LBViewer(int nx, int ny);
+		~LBViewer(void);
+
+		virtual int Draw(mglGraph *gr);
+		virtual void Reload(int next);
+
+		virtual void setData(LBD2Q9* lb, real* phi, real* rho, real* U);
+
+	protected:
+		virtual void makeData(void);
+
+		mglData *mPhi, *mRho, *mUx, *mUy;
+		int mXdim, mYdim;
+
+	private:
+		LBD2Q9* lb_;
+		real *phi_, *rho_, *U_;
+
+	};
+
+}
+
+#endif
+
