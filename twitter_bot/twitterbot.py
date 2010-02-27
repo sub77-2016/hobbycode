48334a96aeaf8f8b635cd0fec56f03a5c17b00bd
ngamsaad.waipot <ngamsaad.waipot@ae0df2b8-a31b-11dd-a8c6-677c70d10fe4>
2010-02-27 21:14:09 +0000

    try twitter_bot

--- twitter_bot/twitterbot.py
+++ twitter_bot/twitterbot.py
@@ -29,11 +29,6 @@ password: <twitter_account_password>
 
 BOT_VERSION = "TwitterBot 1.0 (http://mike.verdone.ca/twitter)"
 
-IRC_BOLD = chr(0x02)
-IRC_ITALIC = chr(0x16)
-IRC_UNDERLINE = chr(0x1f)
-IRC_REGULAR = chr(0x0f)
-
 import sys
 import time
 from dateutil.parser import parse
@@ -44,13 +39,6 @@ import os.path
 
 from api import Twitter, TwitterError
 from util import htmlentitydecode
-
-try:
-    import irclib
-except:
-    raise ImportError(
-        "This module requires python irclib available from "
-        + "http://python-irclib.sourceforge.net/")
 
 def debug(msg):
     # uncomment this for debug text stuff
@@ -99,10 +87,6 @@ class TwitterBot(object):
     def __init__(self, configFilename):
         self.configFilename = configFilename
         self.config = load_config(self.configFilename)
-        #self.irc = irclib.IRC()
-        #self.irc.add_global_handler('privmsg', self.handle_privmsg)
-        #self.irc.add_global_handler('ctcp', self.handle_ctcp)
-        #self.ircServer = self.irc.server()
         self.twitter = Twitter(
             self.config.get('twitter', 'email'),
             self.config.get('twitter', 'password'))
@@ -113,7 +97,6 @@ class TwitterBot(object):
 
     def check_statuses(self):
         debug("In check_statuses")
-	#print "In check_statuses"
         try:
             updates = self.twitter.statuses.friends_timeline()
         except Exception, e:
@@ -121,16 +104,11 @@ class TwitterBot(object):
             traceback.print_exc(file=sys.stderr)
             return
         
-	#print updates
         nextLastUpdate = self.lastUpdate
         for update in updates:
-            #print u"test"
             crt = parse(update['created_at']).utctimetuple()
-            #text = (htmlentitydecode(update['text'].replace('\n', ' ')).encode('utf-8', 'replace'))
             #print crt, self.lastUpdate 
             if (crt > self.lastUpdate):
- 	    #if (True):
-                #print "test"
                 text = (htmlentitydecode(
                     update['text'].replace('\n', ' '))
                     .encode('utf-8', 'replace'))
@@ -143,103 +121,39 @@ class TwitterBot(object):
                     print u"%s: %s" %(
                             update['user']['screen_name'],
                             text.decode('utf-8'))
-                    #self.privmsg_channel(
-                    #    u"=^_^=  %s%s%s %s" %(
-                    #        IRC_BOLD, update['user']['screen_name'],
-                    #        IRC_BOLD, text.decode('utf-8')))
                 
                 nextLastUpdate = crt
             else:
                 break
 
         self.lastUpdate = nextLastUpdate
-	#print "In check_statuses"
         
     def process_events(self):
         debug("In process_events")
-        #self.irc.process_once()
-	#print "In process_events"
-    
-    def handle_privmsg(self, conn, evt):
-        debug('got privmsg')
-        args = evt.arguments()[0].split(' ')
-        try:
-            if (not args):
-                return
-            if (args[0] == 'follow' and args[1:]):
-                self.follow(conn, evt, args[1])
-            elif (args[0] == 'unfollow' and args[1:]):
-                self.unfollow(conn, evt, args[1])
-            else:
-                conn.privmsg(
-                    evt.source().split('!')[0], 
-                    "=^_^= Hi! I'm Twitterbot! you can (follow "
-                    + "<twitter_name>) to make me follow a user or "
-                    + "(unfollow <twitter_name>) to make me stop.")
-        except Exception:
-            traceback.print_exc(file=sys.stderr)
-    
-    def handle_ctcp(self, conn, evt):
-        args = evt.arguments()
-        source = evt.source().split('!')[0]
-        if (args):
-            if args[0] == 'VERSION':
-                conn.ctcp_reply(source, "VERSION " + BOT_VERSION)
-            elif args[0] == 'PING':
-                conn.ctcp_reply(source, "PING")
-            elif args[0] == 'CLIENTINFO':
-                conn.ctcp_reply(source, "CLIENTINFO PING VERSION CLIENTINFO")
-
-    def privmsg_channel(self, msg):
-        return self.ircServer.privmsg(
-            self.config.get('irc', 'channel'), msg.encode('utf-8'))
-            
-    def follow(self, conn, evt, name):
-        userNick = evt.source().split('!')[0]
+                
+    def follow(self, name):
         friends = [x['name'] for x in self.twitter.statuses.friends()]
         debug("Current friends: %s" %(friends))
         if (name in friends):
-            conn.privmsg(
-                userNick,
-                "=O_o= I'm already following %s." %(name))
+            print "=O_o= I'm already following %s." %(name)
         else:
             try:
                 self.twitter.friendships.create(id=name)
             except TwitterError:
-                conn.privmsg(
-                    userNick,
-                    "=O_o= I can't follow that user. Are you sure the name is correct?")
+                print "=O_o= I can't follow that user. Are you sure the name is correct?"
                 return
-            conn.privmsg(
-                userNick,
-                "=^_^= Okay! I'm now following %s." %(name))
-            self.privmsg_channel(
-                "=o_o= %s has asked me to start following %s" %(
-                    userNick, name))
+            print "^^\" Okay! I'm now following %s." %(name)
     
-    def unfollow(self, conn, evt, name):
-        userNick = evt.source().split('!')[0]
+    def unfollow(self, name):
         friends = [x['name'] for x in self.twitter.statuses.friends()]
         debug("Current friends: %s" %(friends))
         if (name not in friends):
-            conn.privmsg(
-                userNick,
-                "=O_o= I'm not following %s." %(name))
+            print "=O_o= I'm not following %s." %(name)
         else:
             self.twitter.friendships.destroy(id=name)
-            conn.privmsg(
-                userNick,
-                "=^_^= Okay! I've stopped following %s." %(name))
-            self.privmsg_channel(
-                "=o_o= %s has asked me to stop following %s" %(
-                    userNick, name))
+            print "^^\" Okay! I've stopped following %s." %(name)
     
     def run(self):
-        #self.ircServer.connect(
-            #self.config.get('irc', 'server'), 
-            #self.config.getint('irc', 'port'),
-            #self.config.get('irc', 'nick'))
-        #self.ircServer.join(self.config.get('irc', 'channel'))
 
         while True:
             try:
@@ -258,10 +172,10 @@ def load_config(filename):
     # attempt to read these properties-- they are required
     cp.get('twitter', 'email'),
     cp.get('twitter', 'password')
-    cp.get('irc', 'server')
-    cp.getint('irc', 'port')
-    cp.get('irc', 'nick')
-    cp.get('irc', 'channel')
+    #cp.get('irc', 'server')
+    #cp.getint('irc', 'port')
+    #cp.get('irc', 'nick')
+    #cp.get('irc', 'channel')
 
     return cp
 
